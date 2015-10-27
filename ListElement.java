6f3963f6d8c451bab25dd3585f6950b3f2cad188
OriginalMidas <clayton.brez@gmail.com>
2015-10-26 21:54:43 -0600

    Need to do the comments and error checks/improvements

--- ListElement.java
+++ ListElement.java
@@ -4,7 +4,7 @@ FILENAME: ListElement.java
 DESCRIPTION: 
 */
 package ense374.lab.pkg3;
-
+import java.util.Arrays;
 /**
  *
  * 
@@ -30,6 +30,12 @@ public class ListElement
         this.next = null;
         this.previous = null;
     }
+    public ListElement(int inData)
+    {
+        this.data = inData;
+        this.next = null;
+        this.previous = null;
+    }
     /*
     Purpose: Set the data variable of the ListElement
     Arguements: int - data value to be set
@@ -47,14 +53,18 @@ public class ListElement
     {
        return this.data; 
     }
-    
     /*
-    Purpose: add a ListElement to the 
+    Purpose: add a ListElement to the linked list
     Arguements:
     */
     public void addElement(ListElement le)
     {
-       
+        ListElement currentElement = this;
+        while (currentElement.previous != null)
+        {
+            currentElement = currentElement.previous;
+        }
+        currentElement.previous = le;
     }
     /*
     Purpose:
@@ -62,15 +72,14 @@ public class ListElement
     */
     public ListElement getElement(int index)
     {
-        /* 
-        this may have to have the head handed to it and set currentElement = to it.
-        ListElement currentElement;
-        for ( i = 0; i < index; i++)
+        
+        //this may have to have the head handed to it and set currentElement = to it.
+        ListElement currentElement = this;
+        for (int i = 0; i < index; i++)
         {
             currentElement = this.previous;
         }
-        return currentElement
-        */
+        return currentElement;
     }
     
     /*
@@ -79,38 +88,14 @@ public class ListElement
     */
     public void deleteElement(int index)
     {
-        /* 
-        will be overloaded
-        if singly linked list ( if next = null ):
-        ListElement currentElement = getElement(index - 1)
-        ListElement elementBefore = currentElement
-        go to element being deleted: currentElement = currentElement.previous
-        elementBefore.previous = currentElement.previous
-        delete currentElement. 
+        ListElement currentElement = getElement(index - 1);
+        ListElement elementBefore = currentElement;
+        //go to element being deleted: 
+        currentElement = currentElement.previous;
+        elementBefore.previous = currentElement.previous;
+        currentElement = null;
+        System.gc();
         
-        if doubly linked list ( if next != null )
-        ListElement currentElement = getElement(index-1)
-        ListElement elementBefore = currentElement
-        go to element being deleted: currentElement = currentElement.previous
-        elementBefore.previous = currentElement.previous
-        currentElement = currentElement.previous
-        currentElement.next = elementBefore
-        */
-        
-    }
-    
-    /*
-    Purpose:
-    Arguements:
-    */
-    public void printLinkedListFromTail()
-    {
-        
-        /* will be overloaded for singly linked list or doubly linked list
-        if singly linked list - create an array the size of the number of list elements. do same function as printLinkedListFromHead but instead of print 
-        places the values in the array. From here, print the array backwards
-        if doubly linked list - print using "next" pointer. loop until "next" = null
-        */
     }
     
     /*
@@ -119,6 +104,14 @@ public class ListElement
     */
     public void printLinkedListFromHead()
     {
+        ListElement currentElement = this;
+        System.out.print(this.data + " ");
+        while (currentElement.previous != null)
+        {
+            System.out.print(currentElement.previous.getData() + " ");
+            currentElement = currentElement.previous;
+        }
+        System.out.println("\n");
         // simple print using "previous" pointer. loop until "previous" = null
     }
 }
